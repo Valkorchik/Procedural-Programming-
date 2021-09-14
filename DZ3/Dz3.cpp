8d541d5d5b061e9a812dfb190f0d227a4adb9e0e
Nikita <56596530+Valkorchik@users.noreply.github.com>
2021-09-14 17:30:32 +0300

    Mistakes

--- DZ3/Dz3.cpp
+++ DZ3/Dz3.cpp
@@ -12,7 +12,7 @@ int main()
     return 0;
 }*/
 //3.2
-#include <iostream>
+/*#include <iostream>
 using namespace std;
 double S,p,n,r,m;
 int main() {
@@ -21,5 +21,23 @@ int main() {
     r = (p) / 100;
     m = (S * r * pow((1 + r), n)) / (12 * (pow((1 + r), n) - 1));
     cout << p;
+    return 0;
+}*/
+//3.3
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+string line;
+int main()
+{
+    ofstream out("Test.txt");
+    out<<"1111222ffffaaaaa(((((";
+    ifstream inf("Test.txt");
+    while (inf)
+    {
+        getline(inf,line);
+        cout<<line<<endl;
+    }
     return 0;
 }
