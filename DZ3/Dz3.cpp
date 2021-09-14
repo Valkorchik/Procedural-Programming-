793b6f30864c126944e1eecfe38c0306f6c4fe51
Nikita <56596530+Valkorchik@users.noreply.github.com>
2021-09-14 10:06:36 +0300

    Add new HW3

---
+++ DZ3/Dz3.cpp
@@ -0,0 +1,25 @@
+//1
+/*#include <iostream>
+using namespace std;
+double S,p,n,r,m;
+int main()
+{
+    cout<<"Vedite peremene"<<endl;
+    cin>>S>>p>>n;
+    r=(p)/100;
+    m=(S*r*pow((1+r),n))/(12*(pow((1+r),n)-1));
+    cout<<m;
+    return 0;
+}*/
+//2
+/*#include <iostream>
+using namespace std;
+double S,p,n,r,m;
+int main() {
+    cout << "Vedite peremene" << endl;
+    cin >> S >> m>> n;
+    r = (p) / 100;
+    m = (S * r * pow((1 + r), n)) / (12 * (pow((1 + r), n) - 1));
+    cout << p;
+    return 0;
+}*/
---
+++ DZ3/cmake-build-debug/CMakeFiles/DZ3.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DZ3.dir/Dz3.cpp.obj"
+  "DZ3.exe"
+  "DZ3.exe.manifest"
+  "DZ3.lib"
+  "DZ3.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DZ3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
