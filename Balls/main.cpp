8ca0336d475571d125dfacba4e193a1fd63c015a
Nikita <nikita2003.ns@gmail.com>
2021-10-19 14:56:59 +0300

    Balls

--- Balls/main.cpp
+++ Balls/main.cpp
@@ -1,5 +1,41 @@
 //Шарики
-/*
+/*#include <iostream>
+using namespace std;
+int n,a[11];
+void perestanovka(int i)
+{
+    for (int i=1;i<11;i++) {
+        if (i==a[i])
+        {
+            cout<<a[i]<< " "<<endl;
+
+        }
+        else
+        {
+            for (int j=i;j<n;++j)
+            {
+                swap(a[i],a[j]);
+                i++;
+                perestanovka(i);
+                i--;
+                swap(a[i],a[j]);
+            }
+
+        }
+    }
+
+}
+int main()
+{
+    cout<<"Enter number of balls:\n";
+    cin>>n;
+    for (int i=1;i<n+1;i++) {
+        a[i] = i;
+    }
+    perestanovka(n);
+
+}*/
+//Шарики 2.0
 #include <iostream>
 #include <algorithm>
 #include <vector>
@@ -7,38 +43,37 @@ using namespace std;
 
 bool proverka(vector<int> ball)
 {
-	for (int i = 0; i < ball.size(); i++)
-	{
-		if (i + 1 == ball[i])
-		{
-			return true;
-		}
-	}
+for (int i = 0; i < ball.size(); i++)
+{
+    if (i + 1 == ball[i])
+    {
+        return true;
+    }
+}
 }
 
 int main()
 {
-	setlocale(0, "");
-	cout << "Введите количество шаров -> ";
-	int n;
-	cin >> n;
-	vector< int > vector;
-	int count = 0;
 
-	for (int i = 1; i <= n; i++)
-	{
-		vector.push_back(i);
-	}
-	do
-	{
-		if (proverka(vector) == true)
-		{
-			count += 1;
-		}
-	}
-	while (next_permutation(vector.begin(), vector.end()));
-	cout << "Количество подходящих вариаций ->" << count << endl;
-	return 0;
+cout << "Введите количество шаров -> ";
+int n;
+cin >> n;
+vector< int > vector;
+int count = 0;
+
+for (int i = 1; i <= n; i++)
+{
+    vector.push_back(i);
+}
+do
+{
+    if (proverka(vector) == true)
+    {
+        count += 1;
+    }
+}
+while (next_permutation(vector.begin(), vector.end()));
+cout << "Количество подходящих вариаций -> " << count << endl;
+return 0;
 }
 
- */
